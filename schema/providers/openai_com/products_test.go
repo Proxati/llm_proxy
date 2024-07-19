0530f2e57e9a6c298c1144a30759bd80b5b48e27
RT <rterhaar@atlanticdynamic.com>
2024-07-19 17:55:04 -0400

    add model cost accounting for gpt-4o-mini (#37)
    * add model cost accounting for gpt-4o-mini
    
    * sort the JSON data, and add a test to make sure it's sorted
    
    * deep copy the slice

--- schema/providers/openai_com/products_test.go
+++ schema/providers/openai_com/products_test.go
@@ -1,6 +1,8 @@
 package openai_com
 
 import (
+	"fmt"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +18,15 @@ func TestLoadEmbeddedDataJSON(t *testing.T) {
 	assert.Nil(t, err, "Expected no error loading data.json, but got an error")
 
 	assert.NotEmpty(t, API_Endpoint_Data, "Expected API_Endpoint_Pricing to be populated, but it was empty")
+
+	// alphabetize the list of products, to confirm the JSON file is sorted correctly
+	for _, endpoint := range API_Endpoint_Data {
+		unSortedProducts := make([]Product, len(endpoint.Products))
+		copy(unSortedProducts, endpoint.Products)
+
+		sort.Slice(endpoint.Products, func(i, j int) bool {
+			return endpoint.Products[i].Name < endpoint.Products[j].Name
+		})
+		assert.Equal(t, unSortedProducts, endpoint.Products, fmt.Sprintf("Expected products to be sorted alphabetically for endpoint: %s", endpoint.URL))
+	}
 }

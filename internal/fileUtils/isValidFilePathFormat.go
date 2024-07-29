40bc8e145f0383c67a387db9a90d43329b5a00d0
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-07-29 00:45:06 -0400

    remove wip function

--- internal/fileUtils/isValidFilePathFormat.go
+++ internal/fileUtils/isValidFilePathFormat.go
@@ -3,14 +3,6 @@ package fileUtils
 import "regexp"
 
 // isValidFilePathFormat checks if the given string is formatted like a file path.
-func isValidFilePathFormat1(path string) bool {
-	// Define a regular expression pattern for file paths
-	var filePathPattern = `^([a-zA-Z]:\\|/)?([^<>:"/\\|?*\n]+/)*([^<>:"/\\|?*\n]+)$`
-	re := regexp.MustCompile(filePathPattern)
-	return re.MatchString(path)
-}
-
-// isValidFilePathFormat checks if the given string is formatted like a file path.
 func isValidFilePathFormat(path string) bool {
 	if path == "" {
 		return false

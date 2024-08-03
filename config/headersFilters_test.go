23c4cc66990fb5e3a237f4cd2a95dcace8f0af63
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-08-02 23:11:00 -0400

    make FilterHeaders variadic

--- config/headersFilters_test.go
+++ config/headersFilters_test.go
@@ -1,31 +1,47 @@
 package config
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewHeaderFilterGroup(t *testing.T) {
+func TestHeaderFilterGroup(t *testing.T) {
 	t.Parallel()
 	headers := []string{"Authorization", "Cookie"}
 	hfg := NewHeaderFilterGroup(t.Name(), headers)
 
+	// Check initialization
 	assert.Equal(t, headers, hfg.Headers, "Headers should be initialized correctly")
 	assert.NotNil(t, hfg.index, "Index should be initialized")
+
+	// Check IsHeaderInGroup method
 	assert.True(t, hfg.IsHeaderInGroup("Authorization"), "Authorization header should be in the index")
 	assert.True(t, hfg.IsHeaderInGroup("Cookie"), "Cookie header should be in the index")
 	assert.False(t, hfg.IsHeaderInGroup("Non-Existent-Header"), "Non-Existent-Header should not be in the index")
-}
 
-func TestIsHeaderInGroup(t *testing.T) {
-	t.Parallel()
-	headers := []string{"Authorization", "Cookie"}
-	hfg := NewHeaderFilterGroup(t.Name(), headers)
+	// Check FilterHeaders method with variadic arguments
+	reqHeaders := http.Header{
+		"Authorization": {"Bearer token"},
+		"Cookie":        {"session_id=123"},
+		"User-Agent":    {"Go-http-client/1.1"},
+		"Accept":        {"*/*"},
+	}
 
-	assert.True(t, hfg.IsHeaderInGroup("Authorization"), "Authorization header should be in the index")
-	assert.True(t, hfg.IsHeaderInGroup("Cookie"), "Cookie header should be in the index")
-	assert.False(t, hfg.IsHeaderInGroup("Non-Existent-Header"), "Non-Existent-Header should not be in the index")
+	// Test filtering with additional headers
+	filteredHeaders := hfg.FilterHeaders(reqHeaders, "User-Agent", "Accept")
+	assert.NotContains(t, filteredHeaders, "Authorization", "Authorization header should be filtered out")
+	assert.NotContains(t, filteredHeaders, "Cookie", "Cookie header should be filtered out")
+	assert.NotContains(t, filteredHeaders, "User-Agent", "User-Agent header should be filtered out")
+	assert.NotContains(t, filteredHeaders, "Accept", "Accept header should be filtered out")
+
+	// Test filtering without additional headers
+	filteredHeaders = hfg.FilterHeaders(reqHeaders)
+	assert.NotContains(t, filteredHeaders, "Authorization", "Authorization header should be filtered out")
+	assert.NotContains(t, filteredHeaders, "Cookie", "Cookie header should be filtered out")
+	assert.Contains(t, filteredHeaders, "User-Agent", "User-Agent header should not be filtered out")
+	assert.Contains(t, filteredHeaders, "Accept", "Accept header should not be filtered out")
 }
 
 func TestNewHeaderFiltersContainer(t *testing.T) {

c38622eb8b0ab19ed5ea8059c626f57dadb5b7df
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-08-06 02:15:11 -0400

    quick cleanup and consolidation for the logSourceConfig

--- config/trafficLogger.go
+++ config/trafficLogger.go
@@ -1,5 +1,36 @@
 package config
 
+import (
+	"encoding/json"
+	"log/slog"
+)
+
+// LogSourceConfig holds the configuration toggles for logging request and response data
+type LogSourceConfig struct {
+	LogConnectionStats bool
+	LogRequestHeaders  bool
+	LogRequest         bool
+	LogResponseHeaders bool
+	LogResponse        bool
+}
+
+func (l *LogSourceConfig) String() string {
+	bytes, err := json.Marshal(l)
+	if err != nil {
+		slog.Error("Could not load LogSourceConfig", "error", err)
+		return ""
+	}
+	return string(bytes)
+}
+
+var LogSourceConfigAllTrue = LogSourceConfig{
+	LogConnectionStats: true,
+	LogRequestHeaders:  true,
+	LogRequest:         true,
+	LogResponseHeaders: true,
+	LogResponse:        true,
+}
+
 // trafficLogger stores config filtering options for the *output* of the proxy traffic. To
 // turn off logging of a part of the transaction, if needed.
 type trafficLogger struct {
@@ -10,4 +41,14 @@ type trafficLogger struct {
 	NoLogReqBody     bool      // if true, log request body
 	NoLogRespHeaders bool      // if true, log response headers
 	NoLogRespBody    bool      // if true, log response body
+}
+
+func (t *trafficLogger) GetLogSourceConfig() LogSourceConfig {
+	return LogSourceConfig{
+		LogConnectionStats: !t.NoLogConnStats,
+		LogRequestHeaders:  !t.NoLogReqHeaders,
+		LogRequest:         !t.NoLogReqBody,
+		LogResponseHeaders: !t.NoLogRespHeaders,
+		LogResponse:        !t.NoLogRespBody,
+	}
 }

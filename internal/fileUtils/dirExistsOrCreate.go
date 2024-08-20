99a78a829f95b6bf89aa3145ed579b98c0d88371
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-08-20 16:04:31 -0400

    simplify the dir create helper

--- internal/fileUtils/dirExistsOrCreate.go
+++ internal/fileUtils/dirExistsOrCreate.go
@@ -7,20 +7,8 @@ import (
 
 // DirExistsOrCreate checks if a directory exists, and creates it if it doesn't
 func DirExistsOrCreate(dir string) error {
-	getLogger().Debug("Checking if directory exists", "dirName", dir)
-	_, err := os.Stat(dir)
-	if err != nil {
-		if os.IsNotExist(err) {
-			// If it doesn't exist, create it
-			err := os.MkdirAll(dir, 0750)
-			if err != nil {
-				return fmt.Errorf("failed to create directory: %w", err)
-			}
-			getLogger().Info("Directory created", "dirName", dir)
-		} else {
-			// If os.Stat failed for another reason, return the error
-			return fmt.Errorf("failed to check if directory exists: %w", err)
-		}
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return fmt.Errorf("failed to create directory: %w", err)
 	}
 	return nil
 }

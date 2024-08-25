dab8b07832119e09740b558bdfaca660b9156722
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-08-25 00:18:03 -0400

    add a longer sleep to the proxy e2e test

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -30,7 +30,7 @@ import (
 
 const (
 	// ugly hack to wait for background async
-	defaultSleepTime = 100 * time.Millisecond
+	defaultSleepTime = 200 * time.Millisecond
 	outputSubdir     = "output"
 	certSubdir       = "certs"
 	cacheSubdir      = "cache"

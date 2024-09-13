d209b348cb851919899a48b9ad963ee1f28864b3
RT <rterhaar@atlanticdynamic.com>
2024-09-13 14:43:33 -0400

    relocate the cache staths header consts under the centralized schema package (#89)

--- schema/headers/const.go
+++ schema/headers/const.go
@@ -12,4 +12,10 @@ const (
 
 	// WorkflowName is an optional request header that can be used to specify the name of the workflow
 	WorkflowName = "X-Llm_workflow-name"
+
+	// CacheStatusHeader is a response header that indicates the cache status of the response
+	CacheStatusHeader    = "X-Llm_proxy-Cache"
+	CacheStatusValueHit  = "HIT"
+	CacheStatusValueMiss = "MISS"
+	CacheStatusValueSkip = "SKIP"
 )

065b17b23ed5645697b7cdf73cdafc485a93244d
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-09-14 14:42:01 -0400

    reduce code duplication for helpers

--- proxy/addons/helpers/requestClosed.go
+++ proxy/addons/helpers/requestClosed.go
@@ -8,20 +8,13 @@ import (
 	px "github.com/proxati/mitmproxy/proxy"
 )
 
-// RequestClosedWithCacheSkipHeader is the function used by the Request method when the addon is closed. It doesn't
-// return anything, but instead attaches a 503 response to the flow, and sets a few headers on
-// the response. When the proxy sees the response != nil, it will skip the rest of the addons.
+// RequestClosedWithCacheSkipHeader is the function used by the Request method when the addon is
+// closed. It doesn't return anything, but instead attaches a 503 response to the flow, and sets a
+// few headers on the response. When the proxy sees the response != nil, it will skip the rest of
+// the addons.
 func RequestClosedWithCacheSkipHeader(logger *slog.Logger, f *px.Flow) {
-	logger.WithGroup("closed").Warn("sending a 503 response to client, because this addon is being closed")
-	f.Response = &px.Response{
-		StatusCode: http.StatusServiceUnavailable,
-		Body:       []byte("LLM_Proxy is not available"),
-		Header: http.Header{
-			"Content-Type":            {"text/plain"},
-			headers.CacheStatusHeader: {headers.CacheStatusValueSkip},
-			"Connection":              {"close"},
-		},
-	}
+	RequestClosed(logger, f)
+	f.Response.Header.Set(headers.CacheStatusHeader, headers.CacheStatusValueSkip)
 }
 
 func RequestClosed(logger *slog.Logger, f *px.Flow) {
